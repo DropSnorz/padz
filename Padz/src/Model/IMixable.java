944c8d76d5842e7ea14cf512c43411da36ee2636
Arthur POIRET <dropsnorz@gmail.com>
2016-04-23 21:19:21 +0200

    Decode audio data optimization (process block)

--- Padz/src/Model/IMixable.java
+++ Padz/src/Model/IMixable.java
@@ -6,7 +6,6 @@ import javax.sound.sampled.AudioInputStream;
 
 public interface IMixable {
 
-	
 	public AudioStream getAudioStream();
 	public void addEffect(IEffect effect);
 	public void addEffect(IEffect effect,int i) throws Exception;
